b67547830b5f94d29f242b24efdf4edb9f4a3884
SzypiTBG <krycha3121@gmail.com>
2019-06-28 19:44:41 +0200

    Dodaj pliki projektów.

---
+++ ballTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using PingPongGameW59276;
+
+namespace Pong
+{
+    [TestFixture]
+    class ballTest
+    {
+        [Test]
+        public void scoreTestInitialScoreShouldBeZero()
+        {
+            Player player = new Player();
+            Assert.AreEqual(0, player.score);
+        }
+    }
+}
+
---
+++ scoreTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PingPongGameW59276;
+
+
+namespace Pong
+{
+    [TestFixture]
+    class scoreTest
+    {
+
+
+
+        [Test]
+        public void scoreTestBallShouldAddToScore()
+        {
+            Player player = new Player();
+            Ball ball = new Ball();
+            ball.Score(player);
+
+            Assert.AreEqual(1, player.score);
+        }
+
+    }
+}
